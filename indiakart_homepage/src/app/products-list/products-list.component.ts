de4a4343a724a0a0cc4292d3f2b4a6fc3b7ff416
Chaithu Chowdhary <chaithanyachowdary20@gmail.com>
2024-03-17 14:31:53 -0500

    Initialization of Backend for IndiaKart

--- indiakart_homepage/src/app/products-list/products-list.component.ts
+++ indiakart_homepage/src/app/products-list/products-list.component.ts
@@ -20,16 +20,16 @@ import { ShoppingService } from '../shopping.service';
 })
 export class ProductsListComponent implements OnInit {
 
-  products:Product[] = <Array<Product>>[];
-  images:MyImage[] = <Array<MyImage>>[];
-  cartItems:CartItem[] = <Array<CartItem>>[];
-  searchTerm:any = "";
-  public response:any = JSON.parse(localStorage.getItem("user") || "null");
-  constructor(private _services:ShoppingService, private _prodCatService:OnlineShoppingService , private _router:Router, private activatedRouter:ActivatedRoute) { }
+  products: Product[] = <Array<Product>>[];
+  images: MyImage[] = <Array<MyImage>>[];
+  cartItems: CartItem[] = <Array<CartItem>>[];
+  searchTerm: any = "";
+  public response: any = JSON.parse(localStorage.getItem("user") || "null");
+  constructor(private _services: ShoppingService, private _prodCatService: OnlineShoppingService, private _router: Router, private activatedRouter: ActivatedRoute) { }
 
   ngOnInit(): void {
     this._services.getProducts().subscribe(data => this.products = data);
-    this._services.getCartItems(this.response ? this.response.id:0).subscribe(data => this.cartItems = data);
+    this._services.getCartItems(this.response ? this.response.id : 0).subscribe(data => this.cartItems = data);
 
     this._prodCatService.getAllCategory().subscribe(data => {
       this.CategoryList = data;
@@ -39,35 +39,35 @@ export class ProductsListComponent implements OnInit {
 
     this._prodCatService.getAll().subscribe(data => {
       this.AllProds = data;
-      console.log("all products= "+this.AllProds)
-      this.Prods=this.AllProds;
+      console.log("all products= " + this.AllProds)
+      this.Prods = this.AllProds;
     });
 
 
 
 
   }
-  addCart(prod:ProductCatViewModel){
-    if(this.response == null){
+  addCart(prod: ProductCatViewModel) {
+    if (this.response == null) {
       alert(`Please login or signup first`);
       return
     }
     let existingItem = <CartItem>{};
     this.cartItems.forEach(item => {
-      if (item.productId == prod.productId){
+      if (item.productId == prod.productId) {
         existingItem = item;
       }
     });
-    if (existingItem.id == null){
-    let orderItem = {} as OrderItem;
-    orderItem.cost = prod.price;
-    orderItem.dateAdded = new Date();
-    orderItem.productId = prod.productId;
-    orderItem.quantity = 1;
-    orderItem.userId = this.response.id;
-    this._services.addToCart(orderItem).subscribe();
+    if (existingItem.id == null) {
+      let orderItem = {} as OrderItem;
+      orderItem.cost = prod.price;
+      orderItem.dateAdded = new Date();
+      orderItem.productId = prod.productId;
+      orderItem.quantity = 1;
+      orderItem.userId = this.response.id;
+      this._services.addToCart(orderItem).subscribe();
     }
-    else{
+    else {
       existingItem.quantity += 1;
       existingItem.cost += prod.price;
       this._services.updateCart(existingItem).subscribe();
@@ -78,63 +78,41 @@ export class ProductsListComponent implements OnInit {
 
   }
 
+  Prods: ProductCatViewModel[] = [];
+  filterProds: ProductCatViewModel[] = [];
+  AllProds: ProductCatViewModel[] = [];
 
-
-
-
-
-
-  Prods:ProductCatViewModel[]=[];
-  filterProds:ProductCatViewModel[]=[];
-  AllProds:ProductCatViewModel[]=[];
-
-  tid:number=0;
-  CategoryList:Category[]=[];
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  tid: number = 0;
+  CategoryList: Category[] = [];
 
   //tid:number=catform.productId
 
-  onSubmit(catform:any)
-  {
+  onSubmit(catform: any) {
 
     //this.tid=fun1(catform.catname);
     //console.log(this.tid);
 
 
-    if(catform.catname=="0")
-    {
-      this.Prods=this.AllProds;
+    if (catform.catname == "0") {
+      this.Prods = this.AllProds;
 
     }
 
-    else{
+    else {
 
       this._prodCatService.GetProductsByCat(catform.catname)
-      .subscribe( data =>  {this.Prods = data;
-        //fun1(this.Prods);
+        .subscribe(data => {
+          this.Prods = data;
+          //fun1(this.Prods);
 
-      console.log(this.Prods)});
+          console.log(this.Prods)
+        });
     }
 
 
   }
 
-  onPriceSort()
-  {
+  onPriceSort() {
     //console.log("hi");
     //priceSort(this.Prods);
     this.Prods.sort((a, b) => (a.price > b.price) ? 1 : -1)
@@ -142,51 +120,43 @@ export class ProductsListComponent implements OnInit {
 
   }
 
-  onNameSort()
-  {
+  onNameSort() {
     this.Prods.sort((a, b) => (a.name > b.name) ? 1 : -1)
     console.log(this.Prods);
 
   }
 
 
-  onfilter(filterform:any)
-  {
+  onfilter(filterform: any) {
 
-    this.filterProds=[]
+    this.filterProds = []
 
-    this.Prods.forEach((prod)=>
-    {
+    this.Prods.forEach((prod) => {
 
 
-      if(prod.brand==filterform.brand || filterform.brand=='')
-      {
+      if (prod.brand == filterform.brand || filterform.brand == '') {
         // console.log("anish");
 
 
 
-         if(filterform.p2==''&& filterform.p1=='')
-      {
-        this.filterProds.push(prod);
-      }
+        if (filterform.p2 == '' && filterform.p1 == '') {
+          this.filterProds.push(prod);
+        }
 
-      else if(filterform.p2==''&& <number>filterform.p1<=prod.price)
-      {
-        this.filterProds.push(prod);
-      }
+        else if (filterform.p2 == '' && <number>filterform.p1 <= prod.price) {
+          this.filterProds.push(prod);
+        }
 
 
-      else if(filterform.p1==''&& <number>filterform.p2>=prod.price)
-      {
-        this.filterProds.push(prod);
+        else if (filterform.p1 == '' && <number>filterform.p2 >= prod.price) {
+          this.filterProds.push(prod);
 
-      }
+        }
 
 
-      else if(<number>filterform.p1<=prod.price&& <number>filterform.p2>=prod.price)
-      {
-        this.filterProds.push(prod);
-      }
+        else if (<number>filterform.p1 <= prod.price && <number>filterform.p2 >= prod.price) {
+          this.filterProds.push(prod);
+        }
 
       }
     }
@@ -194,18 +164,12 @@ export class ProductsListComponent implements OnInit {
 
     console.log(this.filterProds);
 
-    this.Prods=this.filterProds
+    this.Prods = this.filterProds
 
   }
 
-
-  resetList()
-  {
-    this.Prods=this.AllProds;
+  resetList() {
+    this.Prods = this.AllProds;
   }
-
-
-
-
 
 }
