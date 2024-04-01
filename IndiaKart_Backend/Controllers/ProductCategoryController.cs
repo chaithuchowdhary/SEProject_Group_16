2d7f48ac94746dc8dfd583562fce9d89449b3f7a
Chaithu Chowdhary <chaithanyachowdary20@gmail.com>
2024-04-01 13:01:52 -0500

    Created Product and Product category API

---
+++ IndiaKart_Backend/Controllers/ProductCategoryController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using IndiaKart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace online_shopping.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductCategoryController : ControllerBase
+    {
+        public ProductCategoryController(ShoppingContext context)
+        {
+            _context = context;
+        }
+
+        public ShoppingContext _context { get; }
+        [HttpGet("{id}")]
+        public ActionResult<IEnumerable<ProductCatViewModel>> Get(int id)
+        {
+            var data = _context.ProductCatViewModel.FromSqlInterpolated($"dbo.Products_Category {id}");
+            return Ok(data);
+
+        }
+
+        [HttpGet()]
+        public ActionResult<IEnumerable<ProductCatViewModel>> GetAll()
+        {
+            var data = _context.ProductCatViewModel.FromSqlInterpolated($"dbo.proc1 ");
+            return Ok(data);
+
+        }
+
+
+        [HttpGet("getcategory")]
+        public IEnumerable<Category> Get()
+        {
+            List<Category> data = _context.Category.ToList();
+            return data;
+
+        }
+
+    }
+}
---
+++ IndiaKart_Backend/Controllers/ProductController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using IndiaKart.Models;
+using System.Collections;
+using System.Linq;
+
+namespace IndiaKart.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public ProductController(ShoppingContext context)
+        {
+            _context = context;
+        }
+
+
+        public ShoppingContext _context { get; }
+
+        [HttpGet]
+        public IEnumerable Get()
+        {
+            var products = _context.Products.ToList();
+            return products;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            //Product prod = _context.Products.FirstOrDefault(p => p.id == id);
+            var prod = _context.ProductDetails.FromSqlInterpolated($"dbo.ProductDetails {id}");
+            if (prod != null)
+            {
+
+                return Ok(prod);
+            }
+            else
+                return NotFound();
+        }
+
+        [HttpGet("/images/{id}")]
+        public ActionResult Images(int id)
+        {
+            var imgs = _context.Images.ToList().FindAll(u => u.ProductId == id);
+
+            if (imgs != null)
+            {
+
+                return Ok(imgs);
+            }
+            else
+                return NotFound();
+        }
+    }
+}
