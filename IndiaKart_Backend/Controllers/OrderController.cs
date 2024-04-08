3542fbc231da56763c1c2686af3414a3be9a79b7
Chaithu Chowdhary <chaithanyachowdary20@gmail.com>
2024-04-08 09:25:02 -0500

    Added order controller for placing order and useraddress controller

---
+++ IndiaKart_Backend/Controllers/OrderController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using IndiaKart.Models;
+using IndiaKart.Models.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IndiaKart.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        public OrderController(ShoppingContext context)
+        {
+            _context = context;
+        }
+        public ShoppingContext _context { get; set; }
+
+        [HttpPost]
+        public ActionResult Post(Order order)
+        {
+            _context.Orders.Add(order);
+            _context.SaveChanges();
+            return Ok(order);
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Order> Get(int id)
+        {
+            var orders = _context.Orders.ToList().FindAll(o => o.UserId == id);
+            return orders;
+        }
+
+        [HttpGet("items/{id}")]
+        public IEnumerable<OrderItemsViewModel> GetItems(int id)
+        {
+            var items = _context.OrderItemsView.FromSqlInterpolated($"dbo.SP_OrderItems {id}").ToList();
+            return items;
+        }
+
+    }
+}
