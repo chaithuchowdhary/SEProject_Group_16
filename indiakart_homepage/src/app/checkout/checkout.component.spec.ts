1ea976b899ccb7d793db6231eb09bb557635d03c
Monika <monika.kunchanapalli@gmail.com>
2024-05-02 18:51:59 -0500

    added test cases

--- indiakart_homepage/src/app/checkout/checkout.component.spec.ts
+++ indiakart_homepage/src/app/checkout/checkout.component.spec.ts
@@ -1,23 +1,87 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { NgForm } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, of } from 'rxjs';
 import { CheckoutComponent } from './checkout.component';
+import { ShoppingService } from '../shopping.service';
 
 describe('CheckoutComponent', () => {
   let component: CheckoutComponent;
   let fixture: ComponentFixture<CheckoutComponent>;
+  let router: Router;
+  let shoppingService: ShoppingService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CheckoutComponent ]
-    })
-    .compileComponents();
+      declarations: [CheckoutComponent],
+      imports: [RouterTestingModule],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: {
+                get: () => '1', // Assuming user ID as '1' for testing
+              },
+            },
+          },
+        },
+        {
+          provide: ShoppingService,
+          useValue: {
+            getCartItems: () => of([]), // Mocking empty cart items
+            getAddress: () => of([]), // Mocking empty addresses
+            getCards: () => of([]), // Mocking empty cards
+            deleteCartItem: () => of({}), // Mocking successful deletion of cart item
+            addAddress: () => of({}), // Mocking successful addition of address
+            deleteAddress: () => of({}), // Mocking successful deletion of address
+            addCard: () => of({}), // Mocking successful addition of card
+            deleteCard: () => of({}), // Mocking successful deletion of card
+            addOrder: () => of({}), // Mocking successful order placement
+            updateCart: () => of({}), // Mocking successful cart update after order placement
+          },
+        },
+      ],
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(CheckoutComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
+    shoppingService = TestBed.inject(ShoppingService);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should initialize component properties', () => {
+    expect(component.cartItems).toEqual([]);
+    expect(component.addresses).toEqual([]);
+    expect(component.cards).toEqual([]);
+    expect(component.selectedAddress).toEqual('');
+    expect(component.selectedCard).toEqual('');
+    expect(component.isCollapsed).toBeTrue();
+    expect(component.isCollapsedCard).toBeTrue();
+    expect(component.enteredcvv).toEqual('');
+    // Assuming local storage contains valid user data
+    expect(component.response).toBeTruthy();
+  });
+
+  it('should fetch cart items, addresses, and cards on initialization', () => {
+    spyOn(shoppingService, 'getCartItems').and.returnValue(of([]));
+    spyOn(shoppingService, 'getAddress').and.returnValue(of([]));
+    spyOn(shoppingService, 'getCards').and.returnValue(of([]));
+
+    component.ngOnInit();
+
+    expect(shoppingService.getCartItems).toHaveBeenCalled();
+    expect(shoppingService.getAddress).toHaveBeenCalled();
+    expect(shoppingService.getCards).toHaveBeenCalled();
+  });
+
+  // Add more test cases for other methods and scenarios
 });
--- indiakart_homepage/src/app/compare/compare.component.spec.ts
+++ indiakart_homepage/src/app/compare/compare.component.spec.ts
@@ -1,23 +1,48 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CompareComponent } from './compare.component';
+import { Observable, of } from 'rxjs';
+import { Product } from '../Model/Product';
+import { ShoppingService } from '../shopping.service';
 
 describe('CompareComponent', () => {
   let component: CompareComponent;
   let fixture: ComponentFixture<CompareComponent>;
+  let shoppingService: ShoppingService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CompareComponent ]
-    })
-    .compileComponents();
+      declarations: [CompareComponent],
+      providers: [
+        {
+          provide: ShoppingService,
+          useValue: {
+            getProducts: () => of([]), // Mocking empty product list
+          },
+        },
+      ],
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(CompareComponent);
     component = fixture.componentInstance;
+    shoppingService = TestBed.inject(ShoppingService);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize component properties', () => {
+    expect(component.products).toEqual([]);
+    expect(component.selectedProduct1).toEqual(0);
+    expect(component.selectedProduct2).toEqual(0);
+  });
+
+  it('should fetch products on initialization', () => {
+    spyOn(shoppingService, 'getProducts').and.returnValue(of([]));
+    component.ngOnInit();
+    expect(shoppingService.getProducts).toHaveBeenCalled();
   });
 });
